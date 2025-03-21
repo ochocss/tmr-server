4f622da815e0605f685a15a28c2b6af276bc80ce
Fernando Hirsch <144632964+ochocss@users.noreply.github.com>
2025-03-21 15:21:41 -0300

    Add files via upload

--- create_taskmanager.sql
+++ create_taskmanager.sql
@@ -1,16 +1,29 @@
-drop schema if exists task_manager;
+drop schema if exists tmr;
 
-create schema task_manager;
-use task_manager;
+create schema tmr;
+use tmr;
+
+create table Teachers (
+	ID_teacher int not null auto_increment primary key,
+    Name varchar (80) not null
+);
+
+create table Students (
+	ID_students int not null auto_increment primary key,
+    Name varchar (80) not null
+);
 
 create table Subjects (
 	ID_subject int not null auto_increment primary key,
-    Nome varchar (32) not null unique
+    ID_teacher int not null,
+    Name varchar (32) not null,
+    
+    foreign key (ID_teacher) references Teachers (ID_teacher)
 );
 
 create table TaskTypes (
 	ID_type int not null auto_increment primary key,
-    Nome varchar (16) not null
+    Name varchar (16) not null
 );
 
 create table Tasks (
@@ -19,19 +32,26 @@ create table Tasks (
     ID_subject int not null,
     Descript varchar (64) not null,
     TaskDate date not null,
+    
     foreign key (ID_type) references TaskTypes (ID_type),
     foreign key (ID_subject) references Subjects (ID_subject)
+);
+
+create table TaskStatus (
+	ID_taskStatus int not null auto_increment primary key
 );
 
 insert into TaskTypes values
                           (1, 'Test'), (2, 'Homework'), (3, 'Others');
 
 -- Insert models
-insert into Subjects (Nome) values
-                                ('Art'), ('Biology'), ('Chemistry'), ('Databases'), ('English'),
-                                ('Geography'), ('History'), ('Mathematics'), ('OOP'), ('Philosophy'),
-                                ('Physical Education'), ('Physics'), ('Portuguese'), ('Sociology'),
-                                ('Software Engineering'), ('Other');
+
+insert into Teachers (Name) values ('The Teacher');
+insert into Subjects (ID_teacher, Name) values
+                                (1, 'Art'), (1, 'Biology'), (1, 'Chemistry'), (1, 'Databases'), (1, 'English'),
+                                (1, 'Geography'), (1, 'History'), (1, 'Mathematics'), (1, 'OOP'), (1, 'Philosophy'),
+                                (1, 'Physical Education'), (1, 'Physics'), (1, 'Portuguese'), (1, 'Sociology'),
+                                (1, 'Software Engineering'), (1, 'Other');
 
 create user if not exists 'java'@'localhost' identified by 'password';
-grant all on task_manager.* to 'java'@'localhost';
+grant all on tmr.* to 'java'@'localhost';
